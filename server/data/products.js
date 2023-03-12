d26b809aba058141b5464f978527a5fb95add168
Josh Claxton <jrccode@outlook.com>
2023-03-12 00:11:40 -0600

    initial commit, backend almost ready

---
+++ server/data/products.js
@@ -0,0 +1,12 @@
+const product = [
+  {
+    title: 'Sample Product 1',
+    img: 'image_link',
+    description: 'Basic description here to draw attention',
+    additional:
+      'Fully detailed description here including specifics of the product',
+    price: '25.00',
+    countInStock: '500',
+  },
+]
+export default product
