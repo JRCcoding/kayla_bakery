b8f8a13fe078458e3611302d2dfee352cd484f7c
Josh Claxton <jrccode@outlook.com>
2023-04-17 05:35:59 -0500

    fixes for mobile, new navbar with offcanvas

--- client/src/Components/Navi.js
+++ client/src/Components/Navi.js
@@ -13,7 +13,15 @@ import {
   MDBDropdownMenu,
   MDBDropdownItem,
 } from 'mdb-react-ui-kit'
-import { Nav, Navbar, Container } from 'react-bootstrap'
+import {
+  Nav,
+  Navbar,
+  Container,
+  Button,
+  Form,
+  NavDropdown,
+  Offcanvas,
+} from 'react-bootstrap'
 import { logout } from '../Actions/userActions'
 
 export default function App() {
@@ -30,87 +38,153 @@ export default function App() {
     window.scrollTo(0, 0)
   }, [])
   return (
-    <Navbar
-      className='bg-Navbar text-white font-Lato'
-      expand='xl'
-      collapseOnSelect
-      fixed='top'
-    >
-      <Container>
-        <LinkContainer to='/'>
-          <Navbar.Brand>
-            <h6
-              className='d-inline navtitle font-WindSong text-Navtitle'
-              style={{ zIndex: '9999', marginTop: '10%', marginBottom: '0' }}
-            >
-              Kaylala Kakes
-            </h6>
-          </Navbar.Brand>
-        </LinkContainer>
-        <Navbar.Toggle
-          aria-controls='responsive-navbar-nav'
-          // aria-expanded='false'
-          aria-label='Toggle navigation'
-          onClick={() => setShowBasic(!showBasic)}
-          className='toggler'
-        >
-          🍰
-          {/* 🧁 */}
-          {/* <MDBIcon className='burger' fas icon='birthday-cake' />{' '} */}
-          {/* &#127874; */}
-        </Navbar.Toggle>
-        <Navbar.Collapse id='responsive-navbar-nav'>
-          <Nav className='ml-auto'>
-            {userInfo ? (
-              <MDBDropdown id='username' className='navlink user_nav'>
-                <MDBDropdownToggle className='btn-light'>
-                  {userInfo.name}
-                </MDBDropdownToggle>
-                <MDBDropdownMenu>
-                  <LinkContainer to='/profile'>
-                    <MDBDropdownItem className='font-thin mx-3'>
-                      Profile
-                    </MDBDropdownItem>
-                  </LinkContainer>
-                  <MDBDropdownItem
-                    onClick={logoutHandler}
-                    className='font-thin mx-3'
-                  >
-                    Logout
-                  </MDBDropdownItem>
-                </MDBDropdownMenu>
-              </MDBDropdown>
-            ) : (
-              <LinkContainer to='/login'>
-                <MDBNavbarItem className='navlink '>
-                  <i className='fas fa-user'></i> Sign In
-                </MDBNavbarItem>
-              </LinkContainer>
-            )}
-            {userInfo && userInfo.isAdmin && (
-              <LinkContainer to='/admin/requestlist'>
-                <MDBNavbarItem className='navlink'>Requests</MDBNavbarItem>
-              </LinkContainer>
-            )}
+    // <Navbar
+    //   className='bg-Navbar text-white font-Lato'
+    //   expand='xl'
+    //   collapseOnSelect
+    //   fixed='top'
+    // >
+    //   <Container>
+    //     <LinkContainer to='/'>
+    //       <Navbar.Brand>
+    //         <h6
+    //           className='d-inline navtitle font-WindSong text-Navtitle'
+    //           style={{ zIndex: '9999', marginTop: '10%', marginBottom: '0' }}
+    //         >
+    //           Kaylala Kakes
+    //         </h6>
+    //       </Navbar.Brand>
+    //     </LinkContainer>
+    //     <Navbar.Toggle
+    //       aria-controls='responsive-navbar-nav'
+    //       // aria-expanded='false'
+    //       aria-label='Toggle navigation'
+    //       onClick={() => setShowBasic(!showBasic)}
+    //       className='toggler'
+    //     >
+    //       🍰
+    //       {/* 🧁 */}
+    //       {/* <MDBIcon className='burger' fas icon='birthday-cake' />{' '} */}
+    //       {/* &#127874; */}
+    //     </Navbar.Toggle>
+    //     <Navbar.Collapse id='responsive-navbar-nav'>
+    //       <Nav className='ml-auto'>
+    //         {userInfo ? (
+    //           <MDBDropdown id='username' className='navlink user_nav'>
+    //             <MDBDropdownToggle className='btn-light'>
+    //               {userInfo.name}
+    //             </MDBDropdownToggle>
+    //             <MDBDropdownMenu>
+    //               <LinkContainer to='/profile'>
+    //                 <MDBDropdownItem className='font-thin mx-3'>
+    //                   Profile
+    //                 </MDBDropdownItem>
+    //               </LinkContainer>
+    //               <MDBDropdownItem
+    //                 onClick={logoutHandler}
+    //                 className='font-thin mx-3'
+    //               >
+    //                 Logout
+    //               </MDBDropdownItem>
+    //             </MDBDropdownMenu>
+    //           </MDBDropdown>
+    //         ) : (
+    //           <LinkContainer to='/login'>
+    //             <MDBNavbarItem className='navlink '>
+    //               <i className='fas fa-user'></i> Sign In
+    //             </MDBNavbarItem>
+    //           </LinkContainer>
+    //         )}
+    //         {userInfo && userInfo.isAdmin && (
+    //           <LinkContainer to='/admin/requestlist'>
+    //             <MDBNavbarItem className='navlink'>Requests</MDBNavbarItem>
+    //           </LinkContainer>
+    //         )}
 
-            <LinkContainer to='/products'>
-              <MDBNavbarItem link className='navlink clickable'>
-                Products
-              </MDBNavbarItem>
-            </LinkContainer>
-            {/* <LinkContainer to='/about'>
-              <MDBNavbarItem link className='navlink'>
-                About
-              </MDBNavbarItem>
-            </LinkContainer> */}
-            <LinkContainer to='/contact'>
-              <MDBNavbarItem link className='navlink'>
-                Contact
-              </MDBNavbarItem>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    //         <LinkContainer to='/products'>
+    //           <MDBNavbarItem link className='navlink clickable'>
+    //             Products
+    //           </MDBNavbarItem>
+    //         </LinkContainer>
+    //         {/* <LinkContainer to='/about'>
+    //           <MDBNavbarItem link className='navlink'>
+    //             About
+    //           </MDBNavbarItem>
+    //         </LinkContainer> */}
+    //         <LinkContainer to='/contact'>
+    //           <MDBNavbarItem link className='navlink'>
+    //             Contact
+    //           </MDBNavbarItem>
+    //         </LinkContainer>
+    //       </Nav>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
+    <>
+      {' '}
+      {['lg'].map((expand) => (
+        <Navbar key={expand} bg='#9300d4' expand={expand} className='navbar'>
+          <LinkContainer to='/'>
+            <Navbar.Brand className='d-inline navtitle font-WindSong text-white'>
+              Kaylala Kakes
+            </Navbar.Brand>
+          </LinkContainer>
+
+          <Navbar.Toggle
+            aria-controls={`offcanvasNavbar-expand-${expand}`}
+            className='toggler'
+          >
+            🍰
+          </Navbar.Toggle>
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand-${expand}`}
+            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+            placement='end'
+            style={{ backgroundColor: '#9300d4', color: 'white' }}
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                Kaylala Kakes
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className='justify-content-end flex-grow-1 pe-3'>
+                {userInfo ? (
+                  <NavDropdown
+                    title={userInfo.name}
+                    id={`offcanvasNavbarDropdown-expand-${expand}`}
+                  >
+                    {userInfo.isAdmin === true && (
+                      <>
+                        <NavDropdown.Item href='/admin/requestlist'>
+                          Requests
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                      </>
+                    )}
+                    <NavDropdown.Item href='/profile'>Profile</NavDropdown.Item>
+                    <NavDropdown.Item onClick={logoutHandler}>
+                      Logout
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                ) : (
+                  <></>
+                )}
+
+                <Nav.Link href='/'>
+                  <strong className='navlink'>Home</strong>
+                </Nav.Link>
+                <Nav.Link href='/products'>
+                  <strong className='navlink'>Products</strong>
+                </Nav.Link>
+                <Nav.Link href='/contact'>
+                  <strong className='navlink'>Contact</strong>
+                </Nav.Link>
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Navbar>
+      ))}
+    </>
   )
 }
--- client/src/Screens/homeScreen.js
+++ client/src/Screens/homeScreen.js
@@ -12,26 +12,24 @@ const HomeScreen = () => {
       <Meta title='Kaylala Kakes' />
       <ProductCarousel />
       {/* <ValentineCarousel /> */}
-      <Fade>
-        <Card className='product_card mb-10 mx-10 px-4'>
-          <Col className='text-center'>
-            <p className='text-2xl '>
-              Hi, I’m{' '}
-              <strong className='font-Pacifico'>
-                <h3 className='inline'>Kayla Mikel</h3>
-              </strong>{' '}
-              and I’m a home baker located in Katy, T.X.
-              <br />
-              For as long as I can remember baking has been a love of mine; it’s
-              one of the many things that my mom and I bond over. Watching her
-              decorate was one of my favorite things about it; I stood in awe of
-              her, learning every single detail. She has been my teacher,
-              inspiration, and supporter; because of her I became a cake
-              decorator and started Kaylala Kakes in 2018.
-            </p>
-          </Col>
-        </Card>
-      </Fade>
+      <Card className='product_card mb-10 mx-10 px-4'>
+        <Col className='text-center'>
+          <p className='text-2xl '>
+            Hi, I’m{' '}
+            <strong className='font-Pacifico'>
+              <h3 className='inline'>Kayla Mikel</h3>
+            </strong>{' '}
+            and I’m a home baker located in Katy, T.X.
+            <br />
+            For as long as I can remember baking has been a love of mine; it’s
+            one of the many things that my mom and I bond over. Watching her
+            decorate was one of my favorite things about it; I stood in awe of
+            her, learning every single detail. She has been my teacher,
+            inspiration, and supporter; because of her I became a cake decorator
+            and started Kaylala Kakes in 2018.
+          </p>
+        </Col>
+      </Card>
 
       <Fade up>
         <Contact />
