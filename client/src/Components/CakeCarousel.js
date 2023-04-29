7bff5ff40f716f4c890518a6d1b78344a581f832
Josh Claxton <jrccode@outlook.com>
2023-04-29 00:42:58 -0500

    dual card cake page on mobile looks good

--- client/src/Components/CakeCarousel.js
+++ client/src/Components/CakeCarousel.js
@@ -1,58 +1,29 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Container, Carousel, Image } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import Loader from './Loader'
-import Message from '../Components/Message'
-import { listProducts } from '../Actions/requestActions'
 import '../Styles/Carousel.css'
-import { LinkContainer } from 'react-router-bootstrap'
-import axios from 'axios'
+
+import { cakes } from './products'
 
 const CakeCarousel = () => {
-  const userLogin = useSelector((state) => state.userLogin)
-  const { userInfo } = userLogin
-  const dispatch = useDispatch()
-  // const requestList = useSelector((state) => state.requestList)
-  // const { requests } = requestList
-  const [requests, setRequests] = useState()
-  useEffect(() => {
-    const fetchRequests = async () => {
-      // const config = {
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //     Authorization: `Bearer ${userInfo.token}`,
-      //   },
-      // }
-      const { data } = await axios.get('/api/products')
-      setRequests(data)
-    }
-    fetchRequests()
-  }, [])
-
   return (
-    <Container>
-      <Carousel controls={false} className='request_carousel' interval={999999}>
-        {requests &&
-          requests.slice(0, 13).map((request) => (
-            <Carousel.Item key={request.title}>
-              {request && request.category !== 'cupcake' && (
-                <>
-                  <Image
-                    src={request.img}
-                    alt={request.title}
-                    fluid
-                    className='request_carousel_image'
-                  />
+    <Container className='my-auto'>
+      <Carousel controls={false} className='request_carousel'>
+        {cakes.map((request) => (
+          <Carousel.Item key={request.title}>
+            <Image
+              src={request.img}
+              alt={request.title}
+              fluid
+              className='request_carousel_image'
+            />
 
-                  <Carousel.Caption>
-                    <h2 className='request_carousel_caption font-Pacifico'>
-                      {request.title}
-                    </h2>
-                  </Carousel.Caption>
-                </>
-              )}
-            </Carousel.Item>
-          ))}
+            {/* <Carousel.Caption>
+              <h2 className='request_carousel_caption font-Pacifico'>
+                {request.title}
+              </h2>
+            </Carousel.Caption> */}
+          </Carousel.Item>
+        ))}
       </Carousel>
     </Container>
   )
---
+++ client/src/Components/products.js
@@ -0,0 +1,106 @@
+const cakes = [
+  {
+    title: 'Friends',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cakes_friends.webp',
+    category: 'cake',
+  },
+  {
+    title: 'Flowers',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cakes_flowers.webp',
+    category: 'cake',
+  },
+  {
+    title: 'Dance',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cakes_dancepink.webp',
+    category: 'cake',
+  },
+  {
+    title: 'Bunny',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cakes_bunny.webp',
+    category: 'cake',
+  },
+  {
+    title: '60th',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cakes_60th.webp',
+    category: 'cake',
+  },
+  {
+    title: 'Graduation',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cakes_graduation.webp',
+    category: 'cake',
+  },
+  {
+    title: 'Gryffindor',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cakes_gryffindor.webp',
+    category: 'cake',
+  },
+  {
+    title: 'Jackpot',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cakes_jackpot.webp',
+    category: 'cake',
+  },
+  {
+    title: 'Kitty',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cakes_kitty.webp',
+    category: 'cake',
+  },
+  {
+    title: 'M & M',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cakes_mandm.webp',
+    category: 'cake',
+  },
+  {
+    title: 'Purple Dress',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cakes_purpledress.webp',
+    category: 'cake',
+  },
+  {
+    title: 'Queen',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cakes_queen.webp',
+    category: 'cake',
+  },
+  {
+    title: 'Sonic',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cakes_sonic.webp',
+    category: 'cake',
+  },
+]
+
+const cupcakes = [
+  {
+    title: '25th',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cupcakes_25.webp',
+    category: 'cupcake',
+  },
+  {
+    title: 'Animals',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cupcakes_animals.webp',
+    category: 'cupcake',
+  },
+  {
+    title: 'Butterflies',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cupcakes_butterflies.webp',
+    category: 'cupcake',
+  },
+  {
+    title: 'Flowers',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cupcakes_flowers.webp',
+    category: 'cupcake',
+  },
+  {
+    title: 'Frosty',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cupcakes_frosty.webp',
+    category: 'cupcake',
+  },
+  {
+    title: 'Hearts',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cupcakes_hearts.webp',
+    category: 'cupcake',
+  },
+  {
+    title: 'Oreos',
+    img: 'https://raw.githubusercontent.com/JRCcoding/kayla_bakery/master/client/src/Images/cupcakes_oreos.webp',
+    category: 'cupcake',
+  },
+]
+export { cupcakes, cakes }
